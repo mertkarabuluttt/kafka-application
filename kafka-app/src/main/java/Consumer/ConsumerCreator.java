d3caf2881d8639840936293dadf497fe87260071
mert <mertkarabulut@Merts-MacBook-Pro.local>
2021-04-13 21:14:00 +0300

    Consumer import fix

--- kafka-app/src/main/java/Consumer/ConsumerCreator.java
+++ kafka-app/src/main/java/Consumer/ConsumerCreator.java
@@ -1,12 +1,15 @@
 package Consumer;
 
+
 import Producer.KafkaConstants;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.util.Collections;
 import java.util.Properties;
-import java.util.function.Consumer;
 
 public class ConsumerCreator {
 
@@ -18,10 +21,10 @@ public class ConsumerCreator {
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, KafkaConstants.MAX_POLL_RECORDS);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, IKafkaConstants.OFFSET_RESET_EARLIER);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, KafkaConstants.OFFSET_RESET_EARLIER);
 
         Consumer<Long, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Collections.singletonList(IKafkaConstants.TOPIC_NAME));
+        consumer.subscribe(Collections.singletonList(KafkaConstants.TOPIC_NAME));
         return consumer;
     }
 }

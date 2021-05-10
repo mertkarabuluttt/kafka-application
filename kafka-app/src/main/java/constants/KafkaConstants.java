d48583ba05402bc2712defdadecf373f82f4e741
mert <mertkarabulut17@icloud.com>
2021-05-10 21:52:13 +0300

    Kafka topic name changed

--- kafka-app/src/main/java/constants/KafkaConstants.java
+++ kafka-app/src/main/java/constants/KafkaConstants.java
@@ -5,7 +5,7 @@ public interface KafkaConstants {
     String KAFKA_BROKERS = "localhost:9092";
     Integer MESSAGE_COUNT = 1000;
     String CLIENT_ID="client1";
-    String TOPIC_NAME="test";
+    String TOPIC_NAME="Topic1";
     String GROUP_ID_CONFIG="consumerGroup1";
     Integer MAX_NO_MESSAGE_FOUND_COUNT=100;
     String OFFSET_RESET_LATEST="latest";
